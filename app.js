e74cda65ed3a858c9844c6d78472baa3aef03149
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-19 13:03:57 +0200

    Added delete note functionality

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ class App {
             this.handleFormClick(event);
             this.selectNote(event);
             this.openModal(event);
-            
+            this.deleteNote(event);
         });
 
         document.body.addEventListener('mouseover', event => {
@@ -105,6 +105,8 @@ class App {
     }
 
     openModal(event) {
+        if (event.target.matches('.toolbar-delete')) return;
+
         if (event.target.closest('.note')) {
             this.$modal.classList.toggle('open-modal');
             this.$modalTitle.value = this.title;
@@ -171,6 +173,14 @@ class App {
         this.id = $selectedNote.dataset.id;
     }
     
+    deleteNote(event) {
+        event.stopPropagation();
+        if (!event.target.matches('.toolbar-delete')) return;
+        const id = event.target.dataset.id;
+        this.notes = this.notes.filter(note => note.id !== Number(id));
+        this.displayNotes();
+    }
+
     displayNotes() {
         const hasNotes = this.notes.length > 0;
         this.$placeholder.style.display = hasNotes ? 'none' : 'flex';  
@@ -187,7 +197,7 @@ class App {
                 <div class="toolbar-container">
                     <div class="toolbar">
                     <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette">
-                    <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                    <img class="toolbar-delete" data-id=${note.id} src="https://icon.now.sh/delete">
                     </div>
                 </div>
             </div>
